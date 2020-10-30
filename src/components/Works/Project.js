7591c8b7f7a02d6d7bd0015ad754a47ecddd88cb
Jordi-LG <jordy.leguet@gmail.com>
2020-10-30 12:08:49 +0100

    feat: add latest project GWITH

--- src/components/Works/Project.js
+++ src/components/Works/Project.js
@@ -1,35 +1,31 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import './styles.scss';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
 
-const Project = ({
-  id,
-  title,
-  describe,
-  tags,
-  url,
-  urlGitHub,
-  img,
-}) => {
+const Project = ({ id, title, describe, tags, url, urlGitHub, img }) => {
   const [panelOpen, setPanel] = useState(false);
   const [toggleArrow, setArrow] = useState(false);
 
   const mappedTags = tags.map((tag) => (
-    <li
-      className="works__tag"
-      key={tag}
-    >
+    <li className="works__tag" key={tag}>
       #{tag}
     </li>
   ));
 
-  const panelClasses = panelOpen ? 'works__panel works__panel--open' : 'works__panel';
-  const iconClasses = toggleArrow ? 'works__icon works__icon--open' : 'works__icon';
+  const panelClasses = panelOpen
+    ? "works__panel works__panel--open"
+    : "works__panel";
+  const iconClasses = toggleArrow
+    ? "works__icon works__icon--open"
+    : "works__icon";
 
   return (
     <>
       <li className="works__accordion">
-        <div className="works__img" style={{ backgroundImage: `url(${img})` }}>
+        <div
+          className={`works__img works__img--${id}`}
+          style={{ backgroundImage: `url(${img})` }}
+        >
           <span
             className={iconClasses}
             onClick={() => {
@@ -39,28 +35,32 @@ const Project = ({
           >
             <i className="fas fa-angle-down fa-2x" />
           </span>
-          <h2 className={`works__title works__title--${id}`}>
-            { title }
-          </h2>
+          <h2 className={`works__title works__title--${id}`}>{title}</h2>
         </div>
         <div className={panelClasses}>
-          <p className="works__describe">
-            { describe }
-          </p>
+          <p className="works__describe">{describe}</p>
 
           <hr />
 
-          <ul className="works__tags">
-            {mappedTags}
-          </ul>
+          <ul className="works__tags">{mappedTags}</ul>
 
           <hr />
 
           <div className="works__buttons">
-            <a href={url} rel="noopener noreferrer" target="_blank" className="works__link">
+            <a
+              href={url}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="works__link"
+            >
               <i className="fas fa-globe fa-4x" />
             </a>
-            <a href={urlGitHub} rel="noopener noreferrer" target="_blank" className="projects__github">
+            <a
+              href={urlGitHub}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="projects__github"
+            >
               <i className="fab fa-github fa-4x" />
             </a>
           </div>
--- src/datas/projects.js
+++ src/datas/projects.js
@@ -1,37 +1,50 @@
-import gardien from 'src/assets/images/gardien.png';
-import mariochallenge from 'src/assets/images/mariochallenge.png';
-import portfolio from 'src/assets/images/logoWorks.png';
+import gardien from "src/assets/images/gardien.png";
+import mariochallenge from "src/assets/images/mariochallenge.png";
+import portfolio from "src/assets/images/logoWorks.png";
+import gwith from "src/assets/images/gwith.png";
 
 export default [
   {
     id: 1,
-    title: 'Un gardin près de chez soi',
-    describe: 'Site de recherches et de mises en relation pour la garde de son animal domestique. Projet final The Hacking Project',
-    urlGitHub: 'https://github.com/Jordi-LG/Ungardienpresdechezsoi',
-    url: 'https://ungardienpresdechezsoi.herokuapp.com/',
-    trello: 'https://trello.com/b/ZgBYAvBN/ungardienpretdechezsoi',
-    tags: ['Ruby', 'Javascript', 'Ruby On Rails', 'Bootstrap', 'PostgreSQL'],
+    title: "Un gardin près de chez soi",
+    describe:
+      "Site de recherches et de mises en relation pour la garde de son animal domestique. Projet final The Hacking Project",
+    urlGitHub: "https://github.com/Jordi-LG/Ungardienpresdechezsoi",
+    url: "https://ungardienpresdechezsoi.herokuapp.com/",
+    trello: "https://trello.com/b/ZgBYAvBN/ungardienpretdechezsoi",
+    tags: ["Ruby", "Javascript", "Ruby On Rails", "Bootstrap", "PostgreSQL"],
     img: gardien,
   },
   {
     id: 2,
-    title: 'Exercice Mario',
-    describe: 'Un challenge O\'Clock visuellement adapté en Vue.js',
-    urlGitHub: 'https://github.com/Jordi-LG/challengeMario',
-    url: 'https://challenge-mario.glitch.me/',
-    trello: '',
-    tags: ['Javascript', 'Vue.js', 'Bootstrap'],
+    title: "Exercice Mario",
+    describe: "Un challenge O'Clock visuellement adapté en Vue.js",
+    urlGitHub: "https://github.com/Jordi-LG/challengeMario",
+    url: "https://challenge-mario.glitch.me/",
+    trello: "",
+    tags: ["Javascript", "Vue.js", "Bootstrap"],
     img: mariochallenge,
   },
   {
     id: 3,
-    title: 'Portfolio',
-    describe: 'Mon portfolio réalisé en React/Redux',
-    urlGitHub: 'https://github.com/Jordi-LG/Portfolio',
-    url: 'https://jordi-lg.github.io/',
-    trello: '',
-    tags: ['Javascript', 'React', 'Redux'],
+    title: "Portfolio",
+    describe: "Mon portfolio réalisé en React/Redux",
+    urlGitHub: "https://github.com/Jordi-LG/Portfolio",
+    url: "https://jordi-lg.github.io/",
+    trello: "",
+    tags: ["Javascript", "React", "Redux"],
     img: portfolio,
+  },
+  {
+    id: 4,
+    title: "GWITH - Game Where I'm The Hero",
+    describe:
+      "Application web pour jouer et créer des histoires. Partie front, connecté à une API réalisé avec Symfony",
+    urlGitHub: "https://github.com/O-clock-Hyperion/projet-game-front",
+    url: "http://ec2-34-207-67-75.compute-1.amazonaws.com/",
+    trello: "",
+    tags: ["Javascript", "React", "Redux", "Tailwind"],
+    img: gwith,
   },
   // {
   //   title: 'The Kitten Project',
